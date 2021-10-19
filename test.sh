3aa77129ec45e9c2113d3ae1ed5c9ddb57818de8
Lulzette <ivan@lulzette.ru>
2021-10-19 00:53:17 +0000

    Кое-какие тесты

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl_cmd='curl -s -w "%{http_code}" -o /dev/null'
+url='server:8080'
+$curl_cmd -X POST $url/admin/post/test -F 'body=testpage' && echo ": Create success" || echo ': create fail'
+
+$curl_cmd $url/post/test && echo ": get Success" || echo ': get fail'
+
+$curl_cmd -X POST $url/admin/post/test -F 'body=testpage2' && echo ": Update success" || echo ': update fail'
+
+$curl_cmd -X DELETE $url/admin/post/test && echo ": delete success" || echo ': delete fail'
